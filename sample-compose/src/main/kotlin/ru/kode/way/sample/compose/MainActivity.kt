0f4528b7a2173d3724dbf1926b0a967a61f9a477
Dmitry Suzdalev <dz@kode.ru>
2022-12-29 18:48:41 +0200

    Fix sample compilation

--- sample-compose/src/main/kotlin/ru/kode/way/sample/compose/MainActivity.kt
+++ sample-compose/src/main/kotlin/ru/kode/way/sample/compose/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.ui.Modifier
 import ru.kode.way.Path
 import ru.kode.way.RegionId
 import ru.kode.way.Schema
+import ru.kode.way.Segment
 import ru.kode.way.compose.NodeHost
 import ru.kode.way.sample.compose.app.routing.di.AppFlowComponentImpl
 import ru.kode.way.sample.compose.ui.theme.WayTheme
@@ -25,6 +26,21 @@ class MainActivity : ComponentActivity() {
           val schema = remember {
             object : Schema {
               override val regions: List<RegionId> = listOf(RegionId(Path("app")))
+              override fun children(regionId: RegionId): Set<Segment> {
+                TODO("not implemented")
+              }
+
+              override fun children(regionId: RegionId, segment: Segment): Set<Segment> {
+                TODO("not implemented")
+              }
+
+              override fun targets(regionId: RegionId): Map<Segment, Path> {
+                TODO("not implemented")
+              }
+
+              override fun nodeType(regionId: RegionId, path: Path): Schema.NodeType {
+                TODO("not implemented")
+              }
             }
           }
           NodeHost(schema = schema, nodeBuilder = component.nodeBuilder())
--- sample/src/jvmMain/kotlin/ru/kode/way/sample/Main.kt
+++ sample/src/jvmMain/kotlin/ru/kode/way/sample/Main.kt
@@ -4,6 +4,7 @@ import ru.kode.way.NavigationService
 import ru.kode.way.Path
 import ru.kode.way.RegionId
 import ru.kode.way.Schema
+import ru.kode.way.Segment
 import ru.kode.way.sample.app.routing.di.AppFlowComponentImpl
 
 fun main() {
@@ -12,6 +13,21 @@ fun main() {
   val service = NavigationService(
     object : Schema {
       override val regions: List<RegionId> = listOf(RegionId(Path("app")))
+      override fun children(regionId: RegionId): Set<Segment> {
+        TODO("not implemented")
+      }
+
+      override fun children(regionId: RegionId, segment: Segment): Set<Segment> {
+        TODO("not implemented")
+      }
+
+      override fun targets(regionId: RegionId): Map<Segment, Path> {
+        TODO("not implemented")
+      }
+
+      override fun nodeType(regionId: RegionId, path: Path): Schema.NodeType {
+        TODO("not implemented")
+      }
     },
     component.nodeBuilder(),
   )

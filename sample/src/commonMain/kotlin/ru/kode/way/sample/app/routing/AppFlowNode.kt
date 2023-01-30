5e795d66b4efa045a13923aeac4a6d4edc108daa
Dmitry Suzdalev <dz@kode.ru>
2023-01-30 19:30:22 +0200

    Port KMM sample to use autogenerated targets

--- sample/src/commonMain/kotlin/ru/kode/way/sample/app/routing/AppFlowNode.kt
+++ sample/src/commonMain/kotlin/ru/kode/way/sample/app/routing/AppFlowNode.kt
@@ -4,27 +4,25 @@ import ru.kode.way.Event
 import ru.kode.way.Finish
 import ru.kode.way.FlowNode
 import ru.kode.way.FlowTransition
+import ru.kode.way.Ignore
 import ru.kode.way.NavigateTo
-import ru.kode.way.Path
-import ru.kode.way.Stay
 import ru.kode.way.Target
+import ru.kode.way.sample.app
 import ru.kode.way.sample.core.routing.FlowResult
 
 class AppFlowNode : FlowNode<Event, AppFlowResult> {
   override fun transition(event: Event): FlowTransition<AppFlowResult> {
-    return Stay
+    return Ignore
   }
 
   override val dismissResult: AppFlowResult = AppFlowResult.Dismissed
 
-  override val initial: Target = Path.app.permissions(
-    onFinish = { result ->
-      when (result) {
-        FlowResult.Done -> NavigateTo(Path.app.main)
-        FlowResult.Dismissed -> Finish(AppFlowResult.Dismissed)
-      }
+  override val initial = Target.app.permissions { result ->
+    when (result) {
+      FlowResult.Done -> NavigateTo(Target.app.main(onFinish = { Finish(AppFlowResult.Dismissed) }))
+      FlowResult.Dismissed -> Finish(AppFlowResult.Dismissed)
     }
-  )
+  }
 
   override fun onEntry() {
     TODO("Not yet implemented")
--- sample/src/commonMain/kotlin/ru/kode/way/sample/permissions/routing/PermissionsFlowNode.kt
+++ sample/src/commonMain/kotlin/ru/kode/way/sample/permissions/routing/PermissionsFlowNode.kt
@@ -5,8 +5,9 @@ import ru.kode.way.Finish
 import ru.kode.way.FlowNode
 import ru.kode.way.FlowTransition
 import ru.kode.way.NavigateTo
-import ru.kode.way.Path
+import ru.kode.way.Target
 import ru.kode.way.sample.core.routing.FlowResult
+import ru.kode.way.sample.permissions
 
 sealed interface PermissionsEvent : Event {
   object IntroDone : PermissionsEvent
@@ -14,11 +15,11 @@ sealed interface PermissionsEvent : Event {
 }
 
 class PermissionsFlowNode : FlowNode<PermissionsEvent, FlowResult> {
-  override val initial = Path.permissions.intro
+  override val initial = Target.permissions.intro
 
   override fun transition(event: PermissionsEvent): FlowTransition<FlowResult> {
     return when (event) {
-      PermissionsEvent.IntroDone -> NavigateTo(Path.permissions.request)
+      PermissionsEvent.IntroDone -> NavigateTo(Target.permissions.request)
       PermissionsEvent.AllGranted -> Finish(FlowResult.Dismissed)
     }
   }

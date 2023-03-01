a86ba1d8a56ab9f7336c7adedef067cc1c70a2b1
Dmitry Suzdalev <dz@kode.ru>
2023-03-01 14:08:34 +0200

    Rework building source sets
    This fixes issues with gradle plugin when applied to android project.
    
    It used to add the generated sources dir to all source sets causing
    JUnit to try execute generated files as tests.

--- way-gradle-plugin/src/main/kotlin/ru/kode/way/gradle/WayPlugin.kt
+++ way-gradle-plugin/src/main/kotlin/ru/kode/way/gradle/WayPlugin.kt
@@ -7,6 +7,7 @@ import com.android.build.gradle.api.BaseVariant
 import org.gradle.api.DomainObjectSet
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
@@ -50,50 +51,65 @@ class WayPlugin : Plugin<Project> {
 
     // TODO add "way" dependencies to project (see sqldelight-gradle-plugin for an example)
 
-    val mainSourceSets = findMainSourceSets()
+    val mainSources = findMainSources()
     val mainTask = project.tasks.register("generateWayClasses", GenerateClassesTask::class.java) { task ->
-      configureTask(task, mainSourceSets)
+      configureTask(task, mainSources)
     }
-    mainSourceSets.forEach { sourceSet ->
-      sourceSet.kotlin.srcDir(mainTask)
+    mainSources.forEach { source ->
+      logger.warn("adding to a source set ${source.sourceDirectorySet.kotlin.srcDirs}")
+      source.sourceDirectorySet.kotlin.srcDir(mainTask)
+      source.registerGeneratedDir?.invoke(mainTask)
     }
+
+    val testSources = findTestSources()
+    if (testSources.isNotEmpty()) {
+      val testTask = project.tasks.register("generateTestWayClasses", GenerateClassesTask::class.java) { task ->
+        configureTask(task, testSources)
+      }
+      testSources.forEach { source ->
+        source.sourceDirectorySet.kotlin.srcDir(testTask)
+      }
+    }
+  }
+
+  private fun configureTask(task: GenerateClassesTask, sources: List<Source>) {
+    task.group = "way"
+    sources.forEach { source ->
+      val sourceDirs = source.sourceSets.flatMap { it.kotlin.srcDirs }
+      val waySourceDirs = sourceDirs.map { File(it.parentFile, "way") }
+      task.source(waySourceDirs)
+      task.include("**/*.dot")
+    }
+  }
+
+  private fun Project.findMainSources(): List<Source> {
+    project.extensions.findByType(KotlinMultiplatformExtension::class.java)?.run {
+      return listOf(
+        Source(
+          sourceSets = listOf(sourceSets.getByName("commonMain")),
+          sourceDirectorySet = sourceSets.getByName("commonMain")
+        )
+      )
+    }
+
     (project.extensions.findByName("android") as BaseExtension?)?.run {
       val variants: DomainObjectSet<out BaseVariant> = when (this) {
         is AppExtension -> applicationVariants
         is LibraryExtension -> libraryVariants
-        else -> error("unknown Android plugin $this")
+        else -> error("unexpected android plugin extension $this")
       }
-      variants.forEach { variant ->
-        variant.addJavaSourceFoldersToModel(mainTask.get().outputDirectory)
+      val sourceSets = (project.extensions.findByName("kotlin") as KotlinProjectExtension).sourceSets
+        .associateBy { it.name }
+      return variants.map { variant ->
+        Source(
+          sourceSets = variant.sourceSets.mapNotNull { variantSourceSet -> sourceSets[variantSourceSet.name] },
+          sourceDirectorySet = sourceSets[variant.name]
+            ?: error("failed to find sourceSet for variant ${variant.name}"),
+          registerGeneratedDir = { taskProvider ->
+            variant.addJavaSourceFoldersToModel(taskProvider.get().outputDirectory)
+          }
+        )
       }
-    }
-
-    val testSourceSets = findTestSourceSets()
-    if (testSourceSets.isNotEmpty()) {
-      val testTask = project.tasks.register("generateTestWayClasses", GenerateClassesTask::class.java) { task ->
-        configureTask(task, testSourceSets)
-      }
-      testSourceSets.forEach { sourceSet ->
-        sourceSet.kotlin.srcDir(testTask)
-      }
-    }
-  }
-
-  private fun Project.configureTask(task: GenerateClassesTask, sourceSets: List<KotlinSourceSet>) {
-    task.group = "way"
-    val sourceDirs = sourceSets.flatMap { it.kotlin.srcDirs }
-    val waySourceDirs = sourceDirs.map { File(it.parentFile, "way") }
-    task.source(waySourceDirs)
-    task.include("**/*.dot")
-  }
-
-  private fun Project.findMainSourceSets(): List<KotlinSourceSet> {
-    project.extensions.findByType(KotlinMultiplatformExtension::class.java)?.run {
-      return listOf(sourceSets.getByName("commonMain"))
-    }
-
-    (project.extensions.findByName("android") as BaseExtension?)?.run {
-      return (project.extensions.findByName("kotlin") as KotlinProjectExtension).sourceSets.toList()
     }
 
     // TODO Kotlin-only project
@@ -101,9 +117,14 @@ class WayPlugin : Plugin<Project> {
     return emptyList()
   }
 
-  private fun Project.findTestSourceSets(): List<KotlinSourceSet> {
+  private fun Project.findTestSources(): List<Source> {
     project.extensions.findByType(KotlinMultiplatformExtension::class.java)?.run {
-      return listOf(sourceSets.getByName("commonTest"))
+      return listOf(
+        Source(
+          sourceSets = listOf(sourceSets.getByName("commonTest")),
+          sourceDirectorySet = sourceSets.getByName("commonTest")
+        )
+      )
     }
 
     // TODO Android project
@@ -111,6 +132,12 @@ class WayPlugin : Plugin<Project> {
     return emptyList()
   }
 }
+
+private data class Source(
+  val sourceSets: List<KotlinSourceSet>,
+  val sourceDirectorySet: KotlinSourceSet,
+  val registerGeneratedDir: ((TaskProvider<GenerateClassesTask>) -> Unit)? = null
+)
 
 internal const val LIBRARY_PACKAGE = "ru.kode.way"
 internal const val DEFAULT_SCHEMA_CLASS_NAME = "NavigationSchema"

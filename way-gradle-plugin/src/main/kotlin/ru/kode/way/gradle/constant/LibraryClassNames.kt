ba394d48b24cbbf0d0b0ff050bc4337e1dffe59d
Vladislav Kortikov <vo@kode.ru>
2024-01-26 18:34:18 +0200

    Add library class names to constants

---
+++ way-gradle-plugin/src/main/kotlin/ru/kode/way/gradle/constant/LibraryClassNames.kt
@@ -0,0 +1,18 @@
+package ru.kode.way.gradle.constant
+
+import com.squareup.kotlinpoet.ClassName
+import ru.kode.way.gradle.LIBRARY_PACKAGE
+
+val FLOW_NODE = ClassName(LIBRARY_PACKAGE, "FlowNode")
+val FLOW_TARGET = ClassName(LIBRARY_PACKAGE, "FlowTarget")
+val FLOW_TRANSITION = ClassName(LIBRARY_PACKAGE, "FlowTransition")
+val NODE = ClassName(LIBRARY_PACKAGE, "Node")
+val NODE_BUILDER = ClassName(LIBRARY_PACKAGE, "NodeBuilder")
+val PARALLEL_NODE = ClassName(LIBRARY_PACKAGE, "ParallelNode")
+val PATH = ClassName(LIBRARY_PACKAGE, "Path")
+val REGION_ID = ClassName(LIBRARY_PACKAGE, "RegionId")
+val SCHEMA = ClassName(LIBRARY_PACKAGE, "Schema")
+val SCREEN_NODE = ClassName(LIBRARY_PACKAGE, "ScreenNode")
+val SCREEN_TARGET = ClassName(LIBRARY_PACKAGE, "ScreenTarget")
+val SEGMENT = ClassName(LIBRARY_PACKAGE, "Segment")
+val TARGET = ClassName(LIBRARY_PACKAGE, "Target")

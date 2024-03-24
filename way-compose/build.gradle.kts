98d5b952c640b9aa3bdbdf842f48bbd29314a160
rinekri <rinekri@gmail.com>
2024-03-24 20:03:27 +0200

    Update agp to 8.3.0, gradle wrapper to 8.6 and java to 11

--- way-compose/build.gradle.kts
+++ way-compose/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
 
 android {
   namespace = "ru.kode.way.compose"
+
   compileSdk = 33
 
   defaultConfig {
@@ -19,12 +20,12 @@ android {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
   }
 
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "11"
   }
 
   buildFeatures {
